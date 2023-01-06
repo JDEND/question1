ce65a0400286d61ef39362099c188ef68c9a1fa8
Jared Endicott <Jedijde@gmail.com>
2023-01-05 19:16:15 -0500

    Add project files.

---
+++ question1/main.cpp
@@ -0,0 +1,44 @@
+// Section 20
+// Challenge 1
+// Identifying palindrome strings using a deque
+#include <cctype>
+#include <deque>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+
+bool is_palindrome(const std::string& s)
+{
+    std::deque<char> palind;
+    // You must implement this function.
+    // Since we are learning the STL - use a deque to solve the problem.
+    for (char i : s)
+        if (std::isalpha(i))
+            palind.push_back(std::toupper(i));
+
+    while (palind.size() > 1)
+        if (palind.back() != palind.front())
+            return false;
+        else {
+            palind.pop_front();
+            palind.pop_back();
+        }
+
+    return true;
+}
+
+int main()
+{
+    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
+   
+    std::cout << std::boolalpha;
+    std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
+    for(const auto& s : test_strings) {
+        std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
+    }
+    std::cout << std::endl;
+    return 0;
+}
